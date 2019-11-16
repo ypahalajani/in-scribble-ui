ee248db1d382935dda83c47aedaae3007249a534
Yash Pahalajani <ypahalajani@gmail.com>
2019-11-16 20:14:07 +0530

    refactor: create hello world page

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -14,19 +14,19 @@ import { Switch, Route } from 'react-router-dom';
 import HomePage from 'containers/HomePage/Loadable';
 import FeaturePage from 'containers/FeaturePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
-import Header from 'components/Header';
-import Footer from 'components/Footer';
 
 import GlobalStyle from '../../global-styles';
 
-const AppWrapper = styled.div`
-  max-width: calc(768px + 16px * 2);
-  margin: 0 auto;
-  display: flex;
-  min-height: 100%;
-  padding: 0 16px;
-  flex-direction: column;
-`;
+// const AppWrapper = styled.div`
+//   max-width: calc(768px + 16px * 2);
+//   margin: 0 auto;
+//   display: flex;
+//   min-height: 100%;
+//   padding: 0 16px;
+//   flex-direction: column;
+// `;
+
+const AppWrapper = styled.div``;
 
 export default function App() {
   return (
@@ -37,13 +37,14 @@ export default function App() {
       >
         <meta name="description" content="A React.js Boilerplate application" />
       </Helmet>
-      <Header />
+      {/* <Header /> */}
       <Switch>
-        <Route exact path="/" component={HomePage} />
+        <Route exact path="/" render={() => <>Hello World!</>} />
         <Route path="/features" component={FeaturePage} />
+        <Route path="/home" component={HomePage} />
         <Route path="" component={NotFoundPage} />
       </Switch>
-      <Footer />
+      {/* <Footer /> */}
       <GlobalStyle />
     </AppWrapper>
   );
