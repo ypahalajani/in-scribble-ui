a4abc2078c39bef3ed4b9977088689348aa21f2d
Yash Pahalajani <ypahalajani@gmail.com>
2019-11-16 23:54:17 +0530

    fix: add clear functionality

--- app/containers/MainPage/index.jsx
+++ app/containers/MainPage/index.jsx
@@ -1,20 +1,44 @@
 import React from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 
 const Container = styled.div`
   padding: 16px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  > div {
+    margin-bottom: 8px;
+
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+`;
+
+const ButtonGroup = styled.div`
+  button {
+    margin-right: 8px;
+    &:last-child {
+      margin-right: 0;
+    }
+  }
 `;
 
 class MainPage extends React.PureComponent {
   constructor(props) {
     super(props);
+    this.state = {
+      backendResponse: '',
+    };
   }
 
   componentDidMount() {
     this.inkCanvas = new window.InkCanvas('inkCanvas');
   }
 
-  printResponseInConsole = () => {
+  handleSubmitButton = () => {
     var strokes = this.inkCanvas.strokes;
 
     // TODO: do something about `getLanguageTag`
@@ -29,23 +53,51 @@ class MainPage extends React.PureComponent {
       inkRecognizer.addStroke(stroke.id, strokeString);
     });
 
-    console.log(JSON.parse(inkRecognizer.data()));
+    const requestBody = JSON.parse(inkRecognizer.data());
+
+    console.log(`This is the requestbody:
+    ${JSON.stringify(requestBody, null, 2)}
+    `);
+
+    const basePath = 'https://yahihai.azurewebsites.net/api';
+
+    axios
+      .post(`${basePath}/prescription`, requestBody)
+      .then(response => {
+        this.setState({
+          backendResponse: JSON.stringify(response.data, null, 2),
+        });
+      })
+      .catch(console.log);
+  };
+
+  handleClearButton = () => {
+    debugger;
+    this.inkCanvas.clear();
   };
 
   render() {
     console.log('[Mainpage.jsx] inside render');
     return (
       <Container>
-        <canvas
-          id="inkCanvas"
-          width="700"
-          height="350"
-          style={{
-            border: '2px solid #CCC',
-            touchAction: 'none',
-          }}
-        />
-        <button onClick={this.printResponseInConsole}>See response</button>
+        <div>
+          <canvas
+            id="inkCanvas"
+            width="700"
+            height="350"
+            style={{
+              border: '2px solid #CCC',
+              touchAction: 'none',
+            }}
+          />
+        </div>
+        <ButtonGroup>
+          <button onClick={this.handleSubmitButton}>
+            Let Backend know what you are writing
+          </button>
+          <button onClick={this.handleClearButton}>Clear</button>
+        </ButtonGroup>
+        {this.state.backendResponse && <p>{this.state.backendResponse}</p>}
       </Container>
     );
   }
